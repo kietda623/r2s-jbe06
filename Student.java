c24d6f41d1edb3880d6e530d5a67536196859184
kietda623 <anhkiet240205@gmail.com>
2026-01-07 18:01:00 +0700

    7/1-lab2

--- Student.java
+++ Student.java
@@ -5,4 +5,5 @@ public class Student {
     String address;
     String gender;
     String email;
+
 }
--- java/src/main/java/Student.java
+++
@@ -1,9 +0,0 @@
-public class Student {
-    String id;
-    String name;
-    int age;
-    String address;
-    String gender;
-    String email;
-
-}
